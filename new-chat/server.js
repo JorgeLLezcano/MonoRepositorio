d642840576a283532d227fce17207b6224e772fe
jorgeluislezcano1988 <jorgeluislezcano1988@gmail.com>
2024-10-24 03:45:59 +0000

    se anulan los cambios

--- new-chat/server.js
+++ new-chat/server.js
@@ -14,16 +14,12 @@ const io=new Server(server,{
         minTimeout: 1000, // Tiempo mínimo entre intentos de reconexión (ms)
         maxAttempts: 5 // Número máximo de intentos de reconexión
      }
-    })
-
-
-    let users = {};
-
+    }
+    
+)
 io.on('connection',(socket)=>{
 socket.on('new-user', (name)=>{
-    users[socket.id] = name; 
     io.emit('user-connected', {id: socket.id, name})
-    socket.emit('connected-users', users);
 })
 
 
@@ -31,13 +27,6 @@ socket.on('new-user', (name)=>{
         // const messageId = socket.id;
         io.emit('chat', { id: socket.id, msg: data.msg, name: data.name })
     })
-    
-    socket.on('disconnect', () => {
-        const name = users[socket.id];
-        delete users[socket.id];  // Eliminar el usuario desconectado
-        io.emit('user-disconnected', { id: socket.id, name });  // Notificar a todos que el usuario se desconectó
-    });
-
 })
 app.get('/', (req, res)=>{
     res.sendFile(`${__dirname}/public/index.html`)
