7f66bdd9c58e17c9d61035cd50a4b004076e6b22
jorgeluislezcano1988 <jorgeluislezcano1988@gmail.com>
2025-04-14 03:09:28 +0000

    se agrege class para y js para lograr que el personaje cuando se detenga mire hacia la ultima direccion en la que camino

--- game-pokemon/script.js
+++ game-pokemon/script.js
@@ -6,18 +6,21 @@ masterPoke.classList.remove('walk-front', 'walk-up', 'walk-left', 'walk-right');
   if(e.key==='ArrowDown'){
     positionY+=10
     masterPoke.classList.add('walk-front')
+    masterPoke.classList.add('face-front');
   }else if(e.key==='ArrowUp'){
 
     positionY-=10
     masterPoke.classList.add('walk-up')
+    masterPoke.classList.add('face-up');
   }else if(e.key==='ArrowLeft'){
     
     positionX-=10
     masterPoke.classList.add('walk-left')
-
+    masterPoke.classList.add('face-left');
   }else if(e.key==='ArrowRight'){
     positionX+=10
     masterPoke.classList.add('walk-right')
+    masterPoke.classList.add('face-right');
   }
 
   masterPoke.style.top=`${positionY}px`;
@@ -26,4 +29,14 @@ masterPoke.classList.remove('walk-front', 'walk-up', 'walk-left', 'walk-right');
 
 window.addEventListener('keyup', () => {
   masterPoke.classList.remove('walk-front', 'walk-up', 'walk-left', 'walk-right');
+
+  if (e.key === 'ArrowDown') {
+    masterPoke.classList.add('face-front');
+  } else if (e.key === 'ArrowUp') {
+    masterPoke.classList.add('face-up');
+  } else if (e.key === 'ArrowLeft') {
+    masterPoke.classList.add('face-left');
+  } else if (e.key === 'ArrowRight') {
+    masterPoke.classList.add('face-right');
+  }
 });
