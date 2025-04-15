8c679a2eee796be676c4e8ee16f6866d80461fb4
jorgeluislezcano1988 <jorgeluislezcano1988@gmail.com>
2025-04-15 03:36:09 +0000

    nuevos cambios

--- game-pokemon/script.js
+++ game-pokemon/script.js
@@ -1,8 +1,43 @@
 const masterPoke=document.getElementById('master-poke')
-let positionY=0;
-let positionX=0
+const pika=document.getElementById('pika')
+
+
+let position= getComputedStyle(masterPoke)
+let positionY= parseInt(position.top);
+let positionX= parseInt(position.left);
+
+
+function checkColittion(element1, element2){
+  let rect1=element1.getBoundingClientRect();
+  let rect2=element2.getBoundingClientRect();
+  
+   return !(
+          rect1.top > rect2.bottom ||
+          rect1.bottom < rect2.top ||
+          rect1.left > rect2.right ||
+          rect1.right < rect2.left
+      );
+  
+  }
+  let ifColision=false
+  function hadlerColision(){
+    if(checkColittion(masterPoke,pika )){
+  
+    // masterPoke.style.backgroundColor='red'
+    masterPoke.classList.add('colision')
+  ifColision= true
+    setTimeout(()=>{
+      // masterPoke.style.backgroundColor=''
+      masterPoke.classList.remove('colision')
+      ifColision=false
+    }, 1000)
+  }
+  }
+
 window.addEventListener('keydown',(e)=>{
 masterPoke.classList.remove('walk-front', 'walk-up', 'walk-left', 'walk-right');
+
+masterPoke.classList.remove('face-front', 'face-up', 'face-left', 'face-right');
   if(e.key==='ArrowDown'){
     positionY+=10
     masterPoke.classList.add('walk-front')
@@ -30,13 +65,21 @@ masterPoke.classList.remove('walk-front', 'walk-up', 'walk-left', 'walk-right');
 window.addEventListener('keyup', () => {
   masterPoke.classList.remove('walk-front', 'walk-up', 'walk-left', 'walk-right');
 
-  if (e.key === 'ArrowDown') {
-    masterPoke.classList.add('face-front');
-  } else if (e.key === 'ArrowUp') {
-    masterPoke.classList.add('face-up');
-  } else if (e.key === 'ArrowLeft') {
-    masterPoke.classList.add('face-left');
-  } else if (e.key === 'ArrowRight') {
-    masterPoke.classList.add('face-right');
-  }
+  // if (e.key === 'ArrowDown') {
+  //   masterPoke.classList.add('face-front');
+  // } else if (e.key === 'ArrowUp') {
+  //   masterPoke.classList.add('face-up');
+  // } else if (e.key === 'ArrowLeft') {
+  //   masterPoke.classList.add('face-left');
+  // } else if (e.key === 'ArrowRight') {
+  //   masterPoke.classList.add('face-right');
+  // }
 });
+
+function gameLoop() {
+  hadlerColision();
+  requestAnimationFrame(gameLoop);
+}
+
+// Iniciar el bucle de juego
+gameLoop()
