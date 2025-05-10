8de2e1a4ec8e8064bd772c0d1fc2a47a8b49af7b
jorgeluislezcano1988 <jorgeluislezcano1988@gmail.com>
2025-05-10 03:22:27 +0000

    nuevo proyecto ping pon web

---
+++ ping-pong-web/script.js
@@ -0,0 +1,71 @@
+const table=document.getElementById('table')
+const pad1=document.getElementById('pad1')
+const pad2=document.getElementById('pad2')
+const disk=document.getElementById('disk')
+
+const recTable=table.getBoundingClientRect()
+const rectPad1=pad1.getBoundingClientRect()
+const rectPad2=pad2.getBoundingClientRect()
+let positionY=0;
+
+document.addEventListener('keydown',(e)=>{
+
+if(e.key==='ArrowDown' && positionY < recTable.height - pad1.offsetHeight){
+  positionY+=10
+}else if(e.key==='ArrowUp' && positionY>0){
+  positionY-=10
+}
+pad1.style.top=`${positionY}px`
+pad2.style.top=`${positionY}px`
+})
+
+function checkCollision(obj1, obj2) {
+    let rect1 = obj1.getBoundingClientRect();
+    let rect2 = obj2.getBoundingClientRect();
+
+    return !(
+        rect1.top > rect2.bottom ||
+        rect1.bottom < rect2.top ||
+        rect1.left > rect2.right ||
+        rect1.right < rect2.left
+    );
+}
+
+let x=0
+let y=0
+
+let velY=2
+let velX=2
+
+function moveDisk(){
+
+   if (checkCollision(pad1, disk) || checkCollision(pad2, disk)) {
+    //velX = -velX;
+   const rectDisk = disk.getBoundingClientRect();
+    const rectPad1 = pad1.getBoundingClientRect();
+    const rectPad2 = pad2.getBoundingClientRect();
+
+    if ((rectDisk.top >= rectPad1.top && rectDisk.bottom <= rectPad1.bottom) || 
+        (rectDisk.top >= rectPad2.top && rectDisk.bottom <= rectPad2.bottom)) {
+      velX = -velX;
+    }
+  }
+
+ x += velX;
+ y += velY;
+if(x+disk.offsetWidth>recTable.width || x<0){
+  velX=-velX
+ }
+ if(y+disk.offsetHeight>recTable.height||y <0){
+  velY=-velY
+ }
+
+
+disk.style.top=`${y}px`
+disk.style.left=`${x}px`
+
+ requestAnimationFrame(moveDisk)
+
+
+}
+moveDisk()
