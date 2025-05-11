30d7a4a5a73048b9f633db1522a2fe3e094249fd
jorgeluislezcano1988 <jorgeluislezcano1988@gmail.com>
2025-05-11 03:26:45 +0000

    nuevo componete score

--- ping-pong-web/script.js
+++ ping-pong-web/script.js
@@ -2,18 +2,23 @@ const table=document.getElementById('table')
 const pad1=document.getElementById('pad1')
 const pad2=document.getElementById('pad2')
 const disk=document.getElementById('disk')
+const point=document.getElementById('point')
 
 const recTable=table.getBoundingClientRect()
 const rectPad1=pad1.getBoundingClientRect()
 const rectPad2=pad2.getBoundingClientRect()
+
+
 let positionY=0;
+let score=0
+
 
 document.addEventListener('keydown',(e)=>{
 
 if(e.key==='ArrowDown' && positionY < recTable.height - pad1.offsetHeight){
-  positionY+=10
+  positionY+=20
 }else if(e.key==='ArrowUp' && positionY>0){
-  positionY-=10
+  positionY-=20
 }
 pad1.style.top=`${positionY}px`
 pad2.style.top=`${positionY}px`
@@ -38,19 +43,25 @@ let velY=2
 let velX=2
 
 function moveDisk(){
+   
 
    if (checkCollision(pad1, disk) || checkCollision(pad2, disk)) {
     velX = -velX;
- 
+    score+=1
+     point.innerText='score: '+score
   }
 
  x += velX;
  y += velY;
 if(x+disk.offsetWidth>recTable.width || x<0){
   velX=-velX
+   score -= 1; // Resta un punto
+        point.innerText = 'score: ' + score;
  }
  if(y+disk.offsetHeight>recTable.height||y <0){
   velY=-velY
+  score -= 1; // Resta un punto
+  point.innerText = 'score: ' + score;
  }
 
 
